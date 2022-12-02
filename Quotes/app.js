f203da52bead854fc7266d395b4916f18c2f2d90
Филип Ташкоски <93309577+ftaskoski123@users.noreply.github.com>
2022-12-02 14:25:49 +0100

    Add files via upload

---
+++ Quotes/app.js
@@ -0,0 +1,19 @@
+const getQuote = async function () {
+  let response = await fetch("https://type.fit/api/quotes", { method: "GET" });
+  let quotes = await response.json();
+  console.log(quotes);
+  let indexQuote = Math.floor(Math.random() * quotes.length);
+  let quote = quotes[indexQuote];
+  console.log(quote);
+  return {
+    text: quote.text,
+    author: quote.author,
+  };
+};
+const quote1 = getQuote()
+  .then((quote) => {
+    console.log(quote);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
